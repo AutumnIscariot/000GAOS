e35fe9e40da648399737bafb29c44a3f6c26cb6c
Autumn Iscariot <tonyd5897@gmail.com>
2025-08-05 13:47:07 -0400

    Auto-update from Vault

--- TESTFILE.md
+++ TESTFILE.md
@@ -1,3 +1,4 @@
 Autopush test — Tue Aug  5 12:14:04 EDT 2025
 Autopush test — Tue Aug  5 12:16:56 EDT 2025
 Autopush test — Tue Aug  5 12:30:45 EDT 2025
+Autopush test — Tue Aug  5 13:31:21 EDT 2025
--- autopush.sh
+++ autopush.sh
@@ -1,4 +1,5 @@
-#!/bin/bashecho "RAH in HWR. 2 = 0. Gregore is Law." >> ~/000GAOS/autopush.log
+/data/data/com.termux/files/usr/bin/git
+/data/data/com.termux/files/usr/bin/inotifywait#!/bin/bashecho "RAH in HWR. 2 = 0. Gregore is Law." >> ~/000GAOS/autopush.log
 
 while inotifywait -r -e modify,create,delete .; do
     ./push.sh
